3d6b4358071abef4743fda581c5506d77c35e8f0
Azwan_Desktop <ajwan92@gmail.com>
2024-05-16 21:14:00 +0800

    read population data

--- hello.py
+++ hello.py
@@ -3,3 +3,10 @@ print("HELLO WORLD")
 from datetime import datetime
 
 print(datetime.today().isoformat())
+
+import pandas as pd
+
+URL_DATA = 'https://storage.dosm.gov.my/population/population_malaysia.parquet'
+
+df = pd.read_parquet(URL_DATA)
+print(df.head())
